6b4761815bdfacf1949d30b8fef91982f70c6010
Marcel.Silva <marcelsilva.dev@gmail.com>
2021-11-17 21:02:53 -0300

    Att Exercicios

---
+++ TrabalhandoComDinheiro/Money.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace TrabalhandoComDinheiro
+{
+    internal class Money
+    {
+        private Caelum.Stella.CSharp.Vault.Currency uSD;
+        private int v;
+
+        public Money(Caelum.Stella.CSharp.Vault.Currency uSD, int v)
+        {
+            this.uSD = uSD;
+            this.v = v;
+        }
+
+        public static implicit operator Money(double v)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static implicit operator Money(decimal v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
